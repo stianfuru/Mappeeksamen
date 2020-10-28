285bf9cccd33828de48a3ed2aaaa077838c68b42
Stian Furu <stian.furu97@gmail.com>
2020-10-28 22:52:20 +0100

    Main-test Oppg 2

--- Mappeeksamen/src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ Mappeeksamen/src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -174,7 +174,14 @@ public class EksamenSBinTre<T> {
 
 class main{
     public static void main(String[] args){
-
+        Integer[] a = {4,7,2,9,4,10,8,7,4,6};
+        EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre.antall()); // Utskrift: 10
+        System.out.println(tre.antall(5)); // Utskrift: 0
+        System.out.println(tre.antall(4)); // Utskrift: 3
+        System.out.println(tre.antall(7)); // Utskrift: 2
+        System.out.println(tre.antall(10)); // Utskrift: 1
 
     }
 }
