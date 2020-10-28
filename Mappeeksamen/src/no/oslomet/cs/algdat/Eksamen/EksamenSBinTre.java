e206725f90313356f2d0952f6e1a1770607f07e6
Stian Furu <stian.furu97@gmail.com>
2020-10-28 21:45:13 +0100

    LeggInn
    Her kopierte jeg bare koden fra Programkode 5.2.3 som det sto man kunne gjøre også sa jeg at q var forelder til p. Da funket testene

--- Mappeeksamen/src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ Mappeeksamen/src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -1,10 +1,7 @@
 package no.oslomet.cs.algdat.Eksamen;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class EksamenSBinTre<T> {
     private static final class Node<T>   // en indre nodeklasse
@@ -84,7 +81,28 @@ public class EksamenSBinTre<T> {
     }
 
     public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        Objects.requireNonNull(verdi, "Ulovlig med nullverdier!");
+
+        Node<T> p = rot, q = null;               // p starter i roten
+        int cmp = 0;                             // hjelpevariabel
+
+        while (p != null)       // fortsetter til p er ute av treet
+        {
+            q = p;                                 // q er forelder til p
+            cmp = comp.compare(verdi,p.verdi);     // bruker komparatoren
+            p = cmp < 0 ? p.venstre : p.høyre;     // flytter p
+        }
+
+        // p er nå null, dvs. ute av treet, q er den siste vi passerte
+
+        p = new Node(verdi,q);                   // oppretter en ny node
+
+        if (q == null) rot = p;                  // p blir rotnode
+        else if (cmp < 0) q.venstre = p;         // venstre barn til q
+        else q.høyre = p;                        // høyre barn til q
+
+        antall++;                                // én verdi mer i treet
+        return true;                             // vellykket innlegging
     }
 
     public boolean fjern(T verdi) {
@@ -136,6 +154,7 @@ public class EksamenSBinTre<T> {
 
 class main{
     public static void main(String[] args){
+
 
     }
 }
